a6d9bb0262a6e4dc0190bc5be0ebfde701c601e5
sachinivinodya <vinodyasachinivinodya@gmail.com>
2023-05-10 11:22:20 +0530

    Java_socket_Client

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,5 +1,25 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
+        try{
+
+            Socket socket = new Socket("localhost",65535);
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF("Hello I am a client");
+
+            // to ensure if all data are transfered and closed the stream
+            dataOutputStream.flush();
+            dataOutputStream.close();
+
+            // close the socket
+            socket.close();
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
